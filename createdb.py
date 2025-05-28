f856f443959610b44919fb798fe218049cee659e
nitishgarg26 <66325080+nitishgarg26@users.noreply.github.com>
2025-05-28 10:42:53 +0000

    jac delhi upload

--- createdb.py
+++ createdb.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 # Load CSV
-df = pd.read_csv("jee_data.csv")
+df = pd.read_csv("College Counselling - JAC.csv")
 
 # Ensure ranks are numeric
 df["Closing Rank"] = pd.to_numeric(df["Closing Rank"], errors="coerce")
